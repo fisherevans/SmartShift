820453466d78490cb071907fac0d236ca2e20c38
dfead <Andrewfead@gmail.com>
2014-10-17 00:51:55 +0000

    go fuck yourself, asshole eclipse sql (see #15)

--- smarts/trunk/workspace/ServerAPI/src-sql/create/accounts-table-defs.sql
+++ smarts/trunk/workspace/ServerAPI/src-sql/create/accounts-table-defs.sql
@@ -16,13 +16,13 @@ CREATE TABLE `Accounts`.`User` (
 	`flags` INT(10) NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`),
 	UNIQUE (`email`)
-)
+);
 
 CREATE TABLE `Accounts`.`ContactMethod` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(60),
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`Business` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -33,21 +33,21 @@ CREATE TABLE `Accounts`.`Business` (
 	`inactive` TINYINT(0) NOT NULL DEFAULT 0,
 	`flags` INT(10) NOT NULL DEFAULT 0,
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`Image` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`url` VARCHAR(256) NOT NULL,
 	`alt` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`Server` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`hostname` VARCHAR(256) NOT NULL,
 	`ipAddr` VARCHAR(46) NOT NULL,
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`ApplicationTip` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -55,7 +55,7 @@ CREATE TABLE `Accounts`.`ApplicationTip` (
 	`maxBuildID` INT NOT NULL,
 	`tip` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`Build` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -63,7 +63,7 @@ CREATE TABLE `Accounts`.`Build` (
 	`createTS` DATE NOT NULL,
 	`sqlDir` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`SystemProperty` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -71,48 +71,49 @@ CREATE TABLE `Accounts`.`SystemProperty` (
 	`name` VARCHAR(256) NOT NULL,
 	`propVal` VARCHAR(256), 
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`Preference` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(256),
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`UserContactMethod` (
 	`userID` INT NOT NULL,
 	`cMethodID` INT NOT NULL,
 	`cMethodVal` VARCHAR(60),
 	PRIMARY KEY (`id`)
-)
+);
 
 CREATE TABLE `Accounts`.`UserBusiness` (
 	`userID` INT NOT NULL,
 	`busID` INT NOT NULL,
 	`joinTS` DATE NOT NULL,
 	PRIMARY KEY (`userID`, `busID`)
-)
+);
 
 CREATE TABLE `Accounts`.`BusinessPreference` (
 	`busID` INT NOT NULL,
 	`prefID` INT NOT NULL,
 	`prefVal` VARCHAR(256),
 	PRIMARY KEY (`busID`, `prefID`)
-)
+);
 
 CREATE TABLE `Accounts`.`UserBusinessPreference` (
 	`userBusID` INT NOT NULL,
 	`prefID` INT NOT NULL,
 	`prefVal` VARCHAR(256),
 	PRIMARY KEY (`userBusID`, `prefID`)
-)
+);
 
 ALTER TABLE `Accounts`.`User` 
 ADD CONSTRAINT `user_image`
 	FOREIGN KEY (`imageID`)
 	REFERENCES `Accounts`.`Image` (`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
   
 ALTER TABLE `Accounts`.`Business`
 ADD CONSTRAINT `business_image`
@@ -129,7 +130,8 @@ CONSTRAINT `business_build`
 	FOREIGN KEY (`busID`)
 	REFERENCES `Accounts`.`Business`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 	
 ALTER TABLE `Accounts`.`ApplicationTip`
 ADD CONSTRAINT `applicationtip_maxbuild`
@@ -141,14 +143,16 @@ CONSTRAINT `applicationtip_minbuild`
 	FOREIGN KEY (`minBuildID`)
 	REFERENCES `Accounts`.`Build`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 	
 ALTER TABLE `Accounts`.`SystemProperty`
 ADD CONSTRAINT `systemproperty_server`
 	FOREIGN KEY (`servID`)
 	REFERENCES `Accounts`.`Server`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 
 ALTER TABLE `Accounts`.`UserContactMethod`
 ADD CONSTRAINT `usercontactmethod_user`
@@ -160,7 +164,8 @@ CONSTRAINT `usercontactmethod_cmethod`
 	FOREIGN KEY (`cMethodID`)
 	REFERENCES `Accounts`.`ContactMethod`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 	
 ALTER TABLE `Accounts`.`UserBusinessPreference`
 ADD CONSTRAINT `userbusinesspreference_userbus`
@@ -172,7 +177,8 @@ CONSTRAINT `userbusinesspreference_preference`
 	FOREIGN KEY (`prefID`)
 	REFERENCES `Accounts`.`Preference`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 	
 ALTER TABLE `Accounts`.`UserBusiness`
 ADD CONSTRAINT `userbusiness_user`
@@ -184,7 +190,8 @@ CONSTRAINT `userbusiness_business`
 	FOREIGN KEY (`busID`)
 	REFERENCES `Accounts`.`Business`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+	ON UPDATE NO ACTION
+;
 
 ALTER TABLE `Accounts`.`BusinessPreference`
 ADD CONSTRAINT `businesspreference_business`
@@ -196,7 +203,8 @@ CONSTRAINT `businesspreference_preference`
 	FOREIGN KEY (`prefID`)
 	REFERENCES `Accounts`.`Preference`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;	
+	ON UPDATE NO ACTION
+;	
 
 ALTER TABLE `Accounts`.`UserBusinessPreference`
 ADD CONSTRAINT `userbusinesspreference_userbus`
@@ -208,4 +216,5 @@ CONSTRAINT `userbusinesspreference_preference`
 	FOREIGN KEY (`prefID`)
 	REFERENCES `Accounts`.`Preference`(`id`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;	
+	ON UPDATE NO ACTION
+;	
