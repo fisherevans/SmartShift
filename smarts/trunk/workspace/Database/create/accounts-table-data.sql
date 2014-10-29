70245bc3242d3e5040c7ffb4d634f48fdba4ecfa
Fisher Evans <contact@fisherevans.com>
2014-10-29 13:18:00 +0000

    Added test user to create data script

--- smarts/trunk/workspace/Database/create/accounts-table-data.sql
+++ smarts/trunk/workspace/Database/create/accounts-table-data.sql
@@ -1,3 +1,5 @@
 INSERT INTO `Accounts`.`ContactMethod` (`name`) VALUES ('email');
 INSERT INTO `Accounts`.`ContactMethod` (`name`) VALUES ('phone');
 
+INSERT INTO `Accounts`.`User` (`username`, `passHash`, `email`, `fName`, `lName`, `createTS`)
+VALUES ('testuser', '$2a$10$HxaCFIvtTGuv038oOpL1muLiZZLpjm8MOT2ymDjoYrPYyf3007bym', 'contact+test@fisherevans.com', 'Test', 'User', NOW());
