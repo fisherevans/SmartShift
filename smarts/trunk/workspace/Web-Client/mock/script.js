e2ced7ea603c67dac15081affc4455a1c93c4ad8
Fisher Evans <contact@fisherevans.com>
2014-11-03 15:36:08 +0000

    Updated styles for messages.

--- smarts/trunk/workspace/Web-Client/mock/script.js
+++ smarts/trunk/workspace/Web-Client/mock/script.js
@@ -1,13 +1,20 @@
+var navAvailable = true;
 $(document).ready(function() {
   $(".navElement:not(.current)").on("click", function() {
-    $(this).addClass("noScroll");
-    $(".appWindow").addClass("hidden");
-    $(".navElement").removeClass("current");
-    $(this).delay(250).queue(function() {
-      $(this).removeClass("noScroll");
-      $(".appWindow").removeClass("hidden");
-      $(this).addClass("current");
-      $(this).dequeue();
-    });
+    if(navAvailable) {
+      navAvailable = false;
+      $(this).addClass("noScroll");
+      $(".appWindow").addClass("hidden");
+      $(".navElement").removeClass("current");
+      $(this).delay(250).queue(function() {
+        $(this).removeClass("noScroll");
+        $(".appWindow").removeClass("hidden");
+        $(this).addClass("current");
+        $(this).dequeue();
+      }).delay(250).queue(function() {
+        navAvailable = true;
+        $(this).dequeue();
+      });
+    }
   });
 });
