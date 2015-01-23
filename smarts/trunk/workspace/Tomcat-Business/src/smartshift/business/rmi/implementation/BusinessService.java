f63661d25e6a261ae8001e9c3116b74e52b26550
Fisher Evans <contact@fisherevans.com>
2015-01-23 14:22:38 +0000

    fixed impl of override

--- smarts/trunk/workspace/Tomcat-Business/src/smartshift/business/rmi/implementation/BusinessService.java
+++ smarts/trunk/workspace/Tomcat-Business/src/smartshift/business/rmi/implementation/BusinessService.java
@@ -1,18 +1,20 @@
 package smartshift.business.rmi.implementation;
 
 import java.rmi.RemoteException;
+import org.apache.log4j.Logger;
+import org.dom4j.IllegalAddException;
 import smartshift.common.rmi.BaseRemote;
 import smartshift.common.rmi.interfaces.BusinessServiceInterface;
-import smartshift.common.util.properties.AppConstants;
+import smartshift.common.security.session.UserSession;
+import smartshift.common.security.session.UserSessionManager;
 
 /**
  * @author D. Fisher Evans <contact@fisherevans.com>
  * implementation of the accounts RMI service
  */
 public class BusinessService extends BaseRemote implements BusinessServiceInterface {
-    /**
-     * 
-     */
+    private static Logger logger = Logger.getLogger(BusinessService.class);
+    
     private static final long serialVersionUID = -1647486952709539166L;
 
     /** Initializes the service
@@ -23,26 +25,33 @@ public class BusinessService extends BaseRemote implements BusinessServiceInterf
     }
 
     /**
-     * @see smartshift.common.rmi.interfaces.BusinessServiceInterface#getCost()
+     * @see smartshift.common.rmi.interfaces.BusinessServiceInterface#addUserSession(java.lang.String, java.lang.String, long)
      */
     @Override
-    public double getCost() throws RemoteException {
-        return 100.01;
+    public void addUserSession(String username, String sessionId, long timoutPeriod) throws RemoteException {
+        logger.info("Adding session: " + sessionId);
+        UserSession session = new UserSession(username, sessionId, timoutPeriod);
+        try {
+            UserSessionManager.addSession(session);
+        } catch(IllegalAddException e) {
+            logger.error("Failed to add session: " + sessionId, e);
+            throw new RemoteException("Session already exists", e);
+        }
     }
 
     /**
-     * @see smartshift.common.rmi.interfaces.BaseRemoteInterface#connected(java.lang.String, int)
+     * @see smartshift.common.rmi.interfaces.BusinessServiceInterface#invalidateSession(java.lang.String)
      */
     @Override
-    public void connected(String clientHostname, int clientPort) throws RemoteException {
-        System.out.println("Business:" + AppConstants.CONTEXT_PATH + " says - " + clientHostname + ":" + clientPort + " has connected.");
+    public boolean removeUserSession(String sessionId) {
+        return UserSessionManager.removeSession(sessionId) != null;
     }
 
     /**
-     * @see smartshift.common.rmi.interfaces.BaseRemoteInterface#disconnecting(java.lang.String, int)
+     * @see smartshift.common.rmi.interfaces.BusinessServiceInterface#invalidateAllSessions()
      */
     @Override
-    public void disconnecting(String clientHostname, int clientPort) throws RemoteException {
-        System.out.println("Business:" + AppConstants.CONTEXT_PATH + " says - " + clientHostname + ":" + clientPort + " is disconnecting.");
+    public int invalidateAllUserSessions() {
+        return UserSessionManager.invalidateAllSessions();
     }
 }
