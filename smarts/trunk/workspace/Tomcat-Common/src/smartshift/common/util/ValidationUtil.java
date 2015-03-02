1ffeb42e84d84bb6c444cbb6e78e2e358aabca0b
Fisher Evans <contact@fisherevans.com>
2015-03-02 22:22:35 +0000

    I am not a smart man

--- smarts/trunk/workspace/Tomcat-Common/src/smartshift/common/util/ValidationUtil.java
+++ smarts/trunk/workspace/Tomcat-Common/src/smartshift/common/util/ValidationUtil.java
@@ -6,11 +6,13 @@ public class ValidationUtil {
     private static final SmartLogger logger = new SmartLogger(ValidationUtil.class);
     
     public static String validateName(String name) {
+        logger.debug("validateName() Validating _" + name + "_");
         if(name == null)
             return null;
         name = name.trim().replaceAll(" +", " ");
+        logger.debug("validateName() Trimmed _" + name + "_");
         if(name.length() == 0)
             return null;
-        return null;
+        return name;
     }
 }
