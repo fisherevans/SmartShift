8f6b789ccb3e31f9877271e3afa90c9841d13aa2
cbabcock <crbabcock825@gmail.com>
2015-03-13 14:29:37 +0000

    Repo sync

--- smarts/trunk/workspace/Web-Client/www/js/storefront/directives.js
+++ smarts/trunk/workspace/Web-Client/www/js/storefront/directives.js
@@ -8,4 +8,14 @@ angular.module('storefrontApp', [])
             link: function(){}
         }
 
+    })
+    .directive('ngBlur', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                element.bind('blur', function () {
+                    scope.$apply(attr.ngBlur);
+                })
+            }
+        }
     });
