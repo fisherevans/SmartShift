332c31e96f8e09644e257b606ea41221bb3c98b2
Fisher Evans <contact@fisherevans.com>
2015-04-14 15:15:38 +0000

    updating init scripts for id changes

--- smarts/trunk/workspace/Database/create/business-table-data.sql
+++ smarts/trunk/workspace/Database/create/business-table-data.sql
@@ -11,57 +11,57 @@ INSERT INTO `Capability` (`id`, `name`) VALUES ('24', 'Delete Child Groups');
 INSERT INTO `Capability` (`id`, `name`) VALUES ('26', 'Manage Child Groups');
 INSERT INTO `Capability` (`id`, `name`) VALUES ('999', 'Master Wizard');
 
-INSERT INTO `Group` (`id`, `name`) VALUES ('1', 'Root Group');                   -- 1
-INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('2', '1', 'Kitchen');     -- 2
-INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('3', '1', 'Dining Room'); -- 3
-INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('4', '3', 'Bar');         -- 4
+INSERT INTO `Group` (`id`, `name`) VALUES ('1', 'Root Group');
+INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('2', '1', 'Kitchen');
+INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('3', '1', 'Dining Room');
+INSERT INTO `Group` (`id`, `parentID`, `name`) VALUES ('4', '3', 'Bar');
 
-INSERT INTO `Role` (`id`, `name`) VALUES ('1', 'Management');  -- 1
-INSERT INTO `Role` (`id`, `name`) VALUES ('2', 'Chef');        -- 2
-INSERT INTO `Role` (`id`, `name`) VALUES ('3', 'Dish Washer'); -- 3
-INSERT INTO `Role` (`id`, `name`) VALUES ('4', 'Prep');        -- 4
-INSERT INTO `Role` (`id`, `name`) VALUES ('5', 'Host');        -- 5
-INSERT INTO `Role` (`id`, `name`) VALUES ('6', 'Server');      -- 6
-INSERT INTO `Role` (`id`, `name`) VALUES ('7', 'Cleaning');    -- 7
-INSERT INTO `Role` (`id`, `name`) VALUES ('8', 'Bartender');   -- 8
+INSERT INTO `Role` (`id`, `name`) VALUES ('1', 'Management');
+INSERT INTO `Role` (`id`, `name`) VALUES ('2', 'Chef');
+INSERT INTO `Role` (`id`, `name`) VALUES ('3', 'Dish Washer');
+INSERT INTO `Role` (`id`, `name`) VALUES ('4', 'Prep');
+INSERT INTO `Role` (`id`, `name`) VALUES ('5', 'Host');
+INSERT INTO `Role` (`id`, `name`) VALUES ('6', 'Server');
+INSERT INTO `Role` (`id`, `name`) VALUES ('7', 'Cleaning');
+INSERT INTO `Role` (`id`, `name`) VALUES ('8', 'Bartender');
 
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('1', '1', '1'); -- 1
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('2', '2', '2'); -- 2
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('3', '2', '3'); -- 3
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('4', '2', '4'); -- 4 
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('5', '3', '5'); -- 5 
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('6', '3', '6'); -- 6 
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('7', '3', '7'); -- 7 
-INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('8', '4', '8'); -- 8 
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('1', '1', '1');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('2', '2', '2');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('3', '2', '3');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('4', '2', '4');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('5', '3', '5');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('6', '3', '6');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('7', '3', '7');
+INSERT INTO `GroupRole` (`id`, `grpID`, `roleID`) VALUES ('8', '4', '8');
 
-INSERT INTO `GroupRoleCapability` (`id`, `grpRoleID`, `capID`) VALUES ('1', '999');
+INSERT INTO `GroupRoleCapability` (`grpRoleID`, `capID`) VALUES ('1', '999');
 
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('1', '1', '1', 'Chris', 'Billups');   -- 1
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('2', '2', '2', 'Fisher', 'Evans');    -- 2
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('3', '3', '2', 'Drew', 'Fead');       -- 3
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('4', '4', '3', 'Charlie', 'Babcock'); -- 4
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('5', '5', '3', 'Peter', 'Chapin');    -- 5
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('6', '6', '3', 'Lisa', 'Steinman');   -- 6
-INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('7', '7', '4', 'Nancy', 'Mai');       -- 7
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('1', '1', 'Chris', 'Billups');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('2', '2', 'Fisher', 'Evans');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('3', '2', 'Drew', 'Fead');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('4', '3', 'Charlie', 'Babcock');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('5', '3', 'Peter', 'Chapin');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('6', '3', 'Lisa', 'Steinman');
+INSERT INTO `Employee` (`id`, `defaultGrpID`, `fName`, `lName`) VALUES ('7', '4', 'Nancy', 'Mai');
 
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('1', '1', '1');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('2', '2', '2');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('3', '2', '3');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('4', '3', '4');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('5', '3', '5');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('6', '3', '6');
-INSERT INTO `GroupEmployee` (`id`, `grpID`, `empID`) VALUES ('7', '4', '7');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('1', '1');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('2', '2');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('2', '3');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('3', '4');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('3', '5');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('3', '6');
+INSERT INTO `GroupEmployee` (`grpID`, `empID`) VALUES ('4', '7');
 
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('1', '1', '1');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('2', '1', '2');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('3', '6', '2');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('4', '3', '2');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('5', '4', '2');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('6', '5', '3');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('7', '6', '3');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('8', '7', '3');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('9', '6', '4');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('10', '7', '4');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('11', '7', '5');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('12', '5', '6');
-INSERT INTO `GroupRoleEmployee` (`id`, `grpRoleID`, `empID`) VALUES ('13', '8', '7');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('1', '1');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('1', '2');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('6', '2');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('3', '2');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('4', '2');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('5', '3');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('6', '3');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('7', '3');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('6', '4');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('7', '4');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('7', '5');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('5', '6');
+INSERT INTO `GroupRoleEmployee` (`grpRoleID`, `empID`) VALUES ('8', '7');
--- smarts/trunk/workspace/Database/create/business-table-defs.sql
+++ smarts/trunk/workspace/Database/create/business-table-defs.sql
@@ -6,486 +6,516 @@ COMMIT;
 
 USE Business_1;
 
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE `Group`;
 CREATE TABLE `Group` (
-	`id` INT NOT NULL,
-	`parentID` INT NULL,
-	`name` VARCHAR(45),
-	`active` TINYINT(1) NOT NULL DEFAULT 1,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `parentID` INT NULL,
+  `name` VARCHAR(45),
+  `active` TINYINT(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Role`;
 CREATE TABLE `Role` (
-	`id` INT NOT NULL,
-	`name` VARCHAR(45),
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `name` VARCHAR(45),
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Employee`;
 CREATE TABLE `Employee` (
-	`id` INT NOT NULL,
-	`defaultGrpID` INT NOT NULL,
-	`fName` VARCHAR(60) NULL,
-	`lName` VARCHAR(60) NULL,
-	`active` TINYINT(1) NOT NULL DEFAULT 1,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `defaultGrpID` INT NOT NULL,
+  `fName` VARCHAR(60) NULL,
+  `lName` VARCHAR(60) NULL,
+  `active` TINYINT(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Capability`;
 CREATE TABLE `Capability` (
-	`id` INT NOT NULL,
-	`name` VARCHAR(45),
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `name` VARCHAR(45),
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `GroupRole`;
 CREATE TABLE `GroupRole` (
-	`id` INT NOT NULL,
-	`grpID` INT NOT NULL,
-	`roleID` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `grpID` INT NOT NULL,
+  `roleID` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `GroupEmployee`;
 CREATE TABLE `GroupEmployee` (
-	`grpID` INT NOT NULL,
-	`empID` INT NOT NULL,
-	PRIMARY KEY (`grpID`, `empID`)
+  `grpID` INT NOT NULL,
+  `empID` INT NOT NULL,
+  PRIMARY KEY (`grpID`, `empID`)
 );
 
+DROP TABLE `GroupRoleEmployee`;
 CREATE TABLE `GroupRoleEmployee` (
-	`grpRoleID` INT NOT NULL,
-	`empID` INT NOT NULL,
-	PRIMARY KEY (`grpRoleID`, `empID`)
+  `grpRoleID` INT NOT NULL,
+  `empID` INT NOT NULL,
+  PRIMARY KEY (`grpRoleID`, `empID`)
 );
 
+DROP TABLE `GroupRoleCapability`;
 CREATE TABLE `GroupRoleCapability` (
-	`grpRoleID` INT NOT NULL,
-	`capID` INT NOT NULL,
-	PRIMARY KEY (`grpRoleID`, `capID`)
+  `grpRoleID` INT NOT NULL,
+  `capID` INT NOT NULL,
+  PRIMARY KEY (`grpRoleID`, `capID`)
 );
 
+DROP TABLE `AvailInstance`;
 CREATE TABLE `AvailInstance` (
-	`id` INT NOT NULL,
-	`templateID` INT NOT NULL,
-	`startDate` DATE NOT NULL,
-	`endDate` DATE NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `templateID` INT NOT NULL,
+  `startDate` DATE NOT NULL,
+  `endDate` DATE NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `AvailTemplate`;
 CREATE TABLE `AvailTemplate` (
-	`id` INT NOT NULL,
-	`name` VARCHAR(45),
-	`empID` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `name` VARCHAR(45),
+  `empID` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Availability`;
 CREATE TABLE `Availability` (
-	`id` INT NOT NULL,
-	`templateID` INT NOT NULL,
-	`start` INT NOT NULL,
-	`duration` INT NOT NULL,
-	`repeatEvery` INT NOT NULL DEFAULT 0,
-	`repeatCount` INT NOT NULL DEFAULT 0,
-	`repeatOffset` INT NOT NULL DEFAULT 0,
-	`unavailable` TINYINT(1) NOT NULL DEFAULT 0,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `templateID` INT NOT NULL,
+  `start` INT NOT NULL,
+  `duration` INT NOT NULL,
+  `repeatEvery` INT NOT NULL DEFAULT 0,
+  `repeatCount` INT NOT NULL DEFAULT 0,
+  `repeatOffset` INT NOT NULL DEFAULT 0,
+  `unavailable` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `AvailRepeatWeekly`;
 CREATE TABLE `AvailRepeatWeekly` (
     `id` INT NOT NULL,
-	`availID` INT NOT NULL,
-	`dayOfWeek` INT NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE(`availID`, `dayOfWeek`)
+  `availID` INT NOT NULL,
+  `dayOfWeek` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`availID`, `dayOfWeek`)
 );
 
+DROP TABLE `AvailRepeatMonthlyByDate`;
 CREATE TABLE `AvailRepeatMonthlyByDate` (
     `id` INT NOT NULL,
-	`availID` INT NOT NULL,
-	-- make day of month negative if should start from end
-	`dayOfMonth` INT NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE(`availID`, `dayOfMonth`)
+  `availID` INT NOT NULL,
+  -- make day of month negative if should start from end
+  `dayOfMonth` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`availID`, `dayOfMonth`)
 );
 
+DROP TABLE `AvailRepeatMonthlyByDay`;
 CREATE TABLE `AvailRepeatMonthlyByDay` (
     `id` INT NOT NULL,
-	`availID` INT NOT NULL,
-	`offset` INT NOT NULL DEFAULT 0,
-	-- make day of month negative if should start from end
-	`dayOfWeek` INT NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE(`availID`, `offset`, `dayOfWeek`)
+  `availID` INT NOT NULL,
+  `offset` INT NOT NULL DEFAULT 0,
+  -- make day of month negative if should start from end
+  `dayOfWeek` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`availID`, `offset`, `dayOfWeek`)
 );
 
+DROP TABLE `AvailRepeatYearly`;
 CREATE TABLE `AvailRepeatYearly` (
     `id` INT NOT NULL,
-	`availID` INT NOT NULL,
-	`month` INT NOT NULL,
-	`dayOfMonth` INT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE(`availID`, `month`, `dayOfMonth`)
+  `availID` INT NOT NULL,
+  `month` INT NOT NULL,
+  `dayOfMonth` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`availID`, `month`, `dayOfMonth`)
 );
 
+DROP TABLE `Shift`;
 CREATE TABLE `Shift` (
-	`id` INT NOT NULL,
-	`dayOfWeek` INT NOT NULL,
-	`startTime` TIME NOT NULL,
-	`duration` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `dayOfWeek` INT NOT NULL,
+  `startTime` TIME NOT NULL,
+  `duration` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `SchedTemplateVersion`;
 CREATE TABLE `SchedTemplateVersion` (
-	`id` INT NOT NULL,
-	`schedTempID` INT NOT NULL,
-	`name` VARCHAR(60) NULL,
-	`createTS` TIMESTAMP NOT NULL,
-	`version` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `schedTempID` INT NOT NULL,
+  `name` VARCHAR(60) NULL,
+  `createTS` TIMESTAMP NOT NULL,
+  `version` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Schedule`;
 CREATE TABLE `Schedule` (
-	`id` INT NOT NULL,
-	`schedTempVersionID` INT NOT NULL,
-	`startDate` DATE NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `schedTempVersionID` INT NOT NULL,
+  `startDate` DATE NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `RoleSchedule`;
 CREATE TABLE `RoleSchedule` (
-	`id` INT NOT NULL,
-	`schedID` INT NOT NULL,
-	`grpRoleID` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `schedID` INT NOT NULL,
+  `grpRoleID` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `RoleSchedShift`;
 CREATE TABLE `RoleSchedShift` (
-	`roleSchedID` INT NOT NULL,
-	`shiftID` INT NOT NULL,
-	PRIMARY KEY(`roleSchedID`, `shiftID`)
+  `roleSchedID` INT NOT NULL,
+  `shiftID` INT NOT NULL,
+  PRIMARY KEY(`roleSchedID`, `shiftID`)
 );
 
+DROP TABLE `EmpSchedule`;
 CREATE TABLE `EmpSchedule` (
-	`id` INT NOT NULL,
-	`schedID` INT NOT NULL,
-	`empID` INT NOT NULL,
-	`locked` TINYINT(1) NOT NULL DEFAULT 0,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `schedID` INT NOT NULL,
+  `empID` INT NOT NULL,
+  `locked` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `EmpScheduleShift`;
 CREATE TABLE `EmpScheduleShift` (
-	`empSchedID` INT NOT NULL,
-	`shiftID` INT NOT NULL,
-	`grpRoleID` INT NOT NULL,
-	PRIMARY KEY (`empSchedID`, `shiftID`)
+  `empSchedID` INT NOT NULL,
+  `shiftID` INT NOT NULL,
+  `grpRoleID` INT NOT NULL,
+  PRIMARY KEY (`empSchedID`, `shiftID`)
 );
 
+DROP TABLE `Content`;
 CREATE TABLE `Content` (
-	`id` INT NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `TextContent`;
 CREATE TABLE `TextContent` (
-	`id` INT NOT NULL,
-	`text` VARCHAR(256) NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `text` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `CompoundContent`;
 CREATE TABLE `CompoundContent` (
-	`id` INT NOT NULL,
-	`seqNum` INT NOT NULL,
-	`childID` INT NOT NULL,
-	PRIMARY KEY (`id`, `seqNum`)
+  `id` INT NOT NULL,
+  `seqNum` INT NOT NULL,
+  `childID` INT NOT NULL,
+  PRIMARY KEY (`id`, `seqNum`)
 );
 
+DROP TABLE `StyledContent`;
 CREATE TABLE `StyledContent` (
-	`id` INT NOT NULL,
-	`styleID` INT NOT NULL,
-	PRIMARY KEY (`id`, `styleID`)
+  `id` INT NOT NULL,
+  `styleID` INT NOT NULL,
+  PRIMARY KEY (`id`, `styleID`)
 );
 
+DROP TABLE `Style`;
 CREATE TABLE `Style` (
-	`id` INT NOT NULL,
-	`attribute` VARCHAR(30),
-	`rule` VARCHAR(128),
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `attribute` VARCHAR(30),
+  `rule` VARCHAR(128),
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `TaggedContent`;
 CREATE TABLE `TaggedContent` (
-	`id` INT NOT NULL,
-	`tagID` INT NOT NULL,
-	PRIMARY KEY (`id`, `tagID`)
+  `id` INT NOT NULL,
+  `tagID` INT NOT NULL,
+  PRIMARY KEY (`id`, `tagID`)
 );
 
+DROP TABLE `Tag`;
 CREATE TABLE `Tag` (
-	`id` INT NOT NULL,
-	`name` VARCHAR(128),
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `name` VARCHAR(128),
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `Message`;
 CREATE TABLE `Message` (
-	`id` INT NOT NULL,
-	`parentID` INT NOT NULL,
-	`contentID` INT NOT NULL,
-	`authorID` INT NOT NULL,
-	`sentTS` DATETIME NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL,
+  `parentID` INT NOT NULL,
+  `contentID` INT NOT NULL,
+  `authorID` INT NOT NULL,
+  `sentTS` DATETIME NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE `MessageDelivery`;
 CREATE TABLE `MessageDelivery` (
-	`id` INT NOT NULL,
-	`messageID` INT NOT NULL,
-	`recipientID` INT NOT NULL,
-	`seenTS` DATETIME NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE(`messageID`, `recipientID`)
+  `id` INT NOT NULL,
+  `messageID` INT NOT NULL,
+  `recipientID` INT NOT NULL,
+  `seenTS` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`messageID`, `recipientID`)
 );
 
 ALTER TABLE `Group`
 ADD CONSTRAINT `group_parent`
-	FOREIGN KEY (`parentID`)
-	REFERENCES `Group`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`parentID`)
+  REFERENCES `Group`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `Employee`
 ADD CONSTRAINT `employee_grp`
-	FOREIGN KEY (`defaultGrpID`)
-	REFERENCES `Group`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`defaultGrpID`)
+  REFERENCES `Group`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `GroupRole`
 ADD CONSTRAINT `grouprole_grp`
-	FOREIGN KEY (`grpID`)
-	REFERENCES `Group`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`grpID`)
+  REFERENCES `Group`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `grouprole_role`
-	FOREIGN KEY (`roleID`)
-	REFERENCES `Role`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`roleID`)
+  REFERENCES `Role`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `GroupRoleCapability`
 ADD CONSTRAINT `grouprolecapability_grprole`
-	FOREIGN KEY (`grpRoleID`)
-	REFERENCES `GroupRole`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`grpRoleID`)
+  REFERENCES `GroupRole`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `grouprolecapability_capability`
-	FOREIGN KEY (`capID`)
-	REFERENCES `Capability`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`capID`)
+  REFERENCES `Capability`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `GroupEmployee`
 ADD CONSTRAINT `groupemp_grp`
-	FOREIGN KEY (`grpID`)
-	REFERENCES `Group`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`grpID`)
+  REFERENCES `Group`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `groupemp_emp`
-	FOREIGN KEY (`empID`)
-	REFERENCES `Employee`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`empID`)
+  REFERENCES `Employee`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `GroupRoleEmployee`
 ADD CONSTRAINT `grproleemp_grprole`
-	FOREIGN KEY (`grpRoleID`)
-	REFERENCES `GroupRole`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`grpRoleID`)
+  REFERENCES `GroupRole`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `grproleemp_emp`
-	FOREIGN KEY (`empID`)
-	REFERENCES `Employee`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`empID`)
+  REFERENCES `Employee`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailInstance`
 ADD CONSTRAINT `availinstance_template`
-	FOREIGN KEY (`templateID`)
-	REFERENCES `AvailTemplate` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
-ADD CONSTRAINT `availinstance_emp`
-	FOREIGN KEY (`empID`)
-	REFERENCES `Employee`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`templateID`)
+  REFERENCES `AvailTemplate` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailTemplate`
 ADD CONSTRAINT `availtemplate_emp`
-	FOREIGN KEY (`empID`)
-	REFERENCES `Employee`(`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`empID`)
+  REFERENCES `Employee`(`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `Availability`
 ADD CONSTRAINT `availability_template`
-	FOREIGN KEY (`templateID`)
-	REFERENCES `AvailTemplate` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`templateID`)
+  REFERENCES `AvailTemplate` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailRepeatWeekly`
 ADD CONSTRAINT `repeatweekly_avail`
-	FOREIGN KEY (`availID`)
-	REFERENCES `Availability` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`availID`)
+  REFERENCES `Availability` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailRepeatMonthlyByDate`
 ADD CONSTRAINT `repeatmonthly_avail`
-	FOREIGN KEY (`availID`)
-	REFERENCES `Availability` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`availID`)
+  REFERENCES `Availability` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailRepeatMonthlyByDay`
 ADD CONSTRAINT `repeatmonth_avail`
-	FOREIGN KEY (`availID`)
-	REFERENCES `Availability` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`availID`)
+  REFERENCES `Availability` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `AvailRepeatYearly`
 ADD CONSTRAINT `repeatyearly_avail`
-	FOREIGN KEY (`availID`)
-	REFERENCES `Availability` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`availID`)
+  REFERENCES `Availability` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `Schedule`
 ADD CONSTRAINT `sched_schedtempvers`
-	FOREIGN KEY (`schedTempVersionID`)
-	REFERENCES `SchedTemplateVersion` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`schedTempVersionID`)
+  REFERENCES `SchedTemplateVersion` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `EmpSchedule`
 ADD CONSTRAINT `empsched_sched`
-	FOREIGN KEY (`schedID`)
-	REFERENCES `Schedule` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`schedID`)
+  REFERENCES `Schedule` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `empsched_emp`
-	FOREIGN KEY (`empID`)
-	REFERENCES `Employee` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`empID`)
+  REFERENCES `Employee` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `RoleSchedule`
 ADD CONSTRAINT `rolesched_sched`
-	FOREIGN KEY (`schedID`)
-	REFERENCES `Schedule` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`schedID`)
+  REFERENCES `Schedule` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `rolesched_role`
-	FOREIGN KEY (`grpRoleID`)
-	REFERENCES `GroupRole` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`grpRoleID`)
+  REFERENCES `GroupRole` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `RoleSchedShift`
 ADD CONSTRAINT `roleschedshift_rolesched`
-	FOREIGN KEY (`roleSchedID`)
-	REFERENCES `RoleSchedule` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`roleSchedID`)
+  REFERENCES `RoleSchedule` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `roleschedshift_shift`
-	FOREIGN KEY (`shiftID`)
-	REFERENCES `Shift` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`shiftID`)
+  REFERENCES `Shift` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `TextContent`
 ADD CONSTRAINT `textcontent_content`
-	FOREIGN KEY (`id`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`id`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `CompoundContent`
 ADD CONSTRAINT `compoundcontent_content`
-	FOREIGN KEY (`id`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`id`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `compoundcontent_child`
-	FOREIGN KEY (`childID`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`childID`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `StyledContent`
 ADD CONSTRAINT `styledcontent_content`
-	FOREIGN KEY (`id`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`id`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `styledcontent_style`
-	FOREIGN KEY (`styleID`)
-	REFERENCES `Style` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`styleID`)
+  REFERENCES `Style` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `TaggedContent`
 ADD CONSTRAINT `taggedcontent_content`
-	FOREIGN KEY (`id`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`id`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `taggedcontent_tag`
-	FOREIGN KEY (`tagID`)
-	REFERENCES `Tag` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`tagID`)
+  REFERENCES `Tag` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `Message`
 ADD CONSTRAINT `message_content`
-	FOREIGN KEY (`contentID`)
-	REFERENCES `Content` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`contentID`)
+  REFERENCES `Content` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `message_author`
-	FOREIGN KEY (`authorID`)
-	REFERENCES `Employee` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`authorID`)
+  REFERENCES `Employee` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `message_parent`
-	FOREIGN KEY (`parentID`)
-	REFERENCES `Message` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`parentID`)
+  REFERENCES `Message` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
 
 ALTER TABLE `MessageDelivery`
 ADD CONSTRAINT `mdelivery_recipient`
-	FOREIGN KEY (`recipientID`)
-	REFERENCES `Employee` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION,
+  FOREIGN KEY (`recipientID`)
+  REFERENCES `Employee` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `mdelivery_message`
-	FOREIGN KEY (`messageID`)
-	REFERENCES `Message` (`id`)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION
+  FOREIGN KEY (`messageID`)
+  REFERENCES `Message` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
 ;
+
+SET FOREIGN_KEY_CHECKS=1;
