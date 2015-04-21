20e9d688400b62c729b63736d62b70cae4341b98
Fisher Evans <contact@fisherevans.com>
2015-04-21 11:58:11 +0000

    Enhanced

--- smarts/trunk/workspace/Tomcat-Business/src/smartshift/business/updates/MultiID.java
+++ smarts/trunk/workspace/Tomcat-Business/src/smartshift/business/updates/MultiID.java
@@ -1,32 +1,71 @@
 package smartshift.business.updates;
 
 import java.util.Arrays;
-import smartshift.common.util.log4j.SmartLogger;
 
+/**
+ * @author D. Fisher Evans <contact@fisherevans.com>
+ * @date Apr 21, 2015
+ *
+ * A class to be used as a dynamic/multi ID object
+ */
 public class MultiID {
-    private static final SmartLogger logger = new SmartLogger(MultiID.class);
-    
     private Object[] _ids;
     
+    /** creates the ID object
+     * @param ids
+     */
     public MultiID(Object ... ids) {
         _ids = ids;
     }
 
+    /** 
+     * @return the actual ID array
+     */
     public Object[] getIDs() {
         return _ids;
     }
 
+    /** 
+     * @param position the position of the ID your after
+     * @return the ID value for the position
+     */
+    public Object getID(int position) {
+        return position >= 0 && position < _ids.length ? _ids[position] : null;
+    }
+    
+    /**
+     * @return number of IDs this object uses
+     */
+    public int getLength() {
+        return _ids.length;
+    }
+
+    /** Overridden method - see parent javadoc
+      * @see java.lang.Object#hashCode()
+      */
     @Override
     public int hashCode() {
         return Arrays.hashCode(_ids);
     }
 
+    /** Overridden method - see parent javadoc
+      * @see java.lang.Object#equals(java.lang.Object)
+      */
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof MultiID)
-            return Arrays.equals(_ids, ((MultiID)obj)._ids);
-        return false;
+        if(obj instanceof MultiID) {
+            MultiID other = (MultiID) obj;
+            if(other == null || this.getLength() != other.getLength())
+                return false;
+            Object a, b;
+            for(int position = 0;position < this.getLength();position++) {
+                a = this.getID(position);
+                b = other.getID(position);
+                if((a == null && b != null) || !a.equals(b)) // IF: a is null and b is not -OR- a does not equal b
+                    return false;
+            }
+            return true;
+        } else
+            return false;
     }
-    
-    
 }
